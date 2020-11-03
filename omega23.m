3c55283b33aa61128a7f29efb496ceaa9ffb7463
Tasnim950 <43742297+Tasnim950@users.noreply.github.com>
2020-11-03 13:05:19 -0500

    Constellation Design

---
+++ omega23.m
@@ -0,0 +1,16 @@
+function [omega] = omega23(xr, xi, Nr)
+m = length(xr);
+s = 0;
+
+for i1 = 1:m
+    for i1p = 1:m
+        for i2 = 1:m
+            for i2p = 1:m
+                s = s + (xr(i1)^2 + xr(i2p)^2 + (xr(i2) - xr(i1p))^2 ...
+                    + (xi(i1) - xi(i1p))^2 + xi(i2)^2 + xi(i2p)^2)^(-Nr);
+            end
+        end
+    end
+end
+omega = s; 
+end
---
+++ omega5.m
@@ -0,0 +1,16 @@
+function [omega] = omega5(xr, xi, Nr)
+m = length(xr);
+s = 0; 
+
+for i1 = 1:m
+    for i1p = 1:m
+        for i2 = 1:m
+            for i2p = 1:m
+                s = s + (xr(i1)^2 + xr(i1p)^2 + xr(i2)^2 + xr(i2p)^2 + ...
+                    xi(i1)^2 + xi(i2p)^2 + (xi(i1p) - xi(i2))^2)^(-Nr);
+            end
+        end
+    end
+end
+omega = s;
+end
